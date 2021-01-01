641fe45050debae39622c79f6d8b76043d7684eb
mnosinov <mnosinov@gmail.com>
2021-01-02 00:14:43 +0600

    test func added

--- news/views.py
+++ news/views.py
@@ -2,6 +2,8 @@ from django.http import HttpResponse
 
 
 def index(request):
-    print(request)
-    print(dir(request))
     return HttpResponse('Hello world!')
+
+
+def test(request):
+    return HttpResponse('<h1>Тестовая страница</h1>')
