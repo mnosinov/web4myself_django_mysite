09fd43d138dbee06a59a652e67d8308105eb84b6
mnosinov <mnosinov@gmail.com>
2021-01-04 20:44:09 +0600

    News model has been customized to show information in admin panel more informative

--- news/views.py
+++ news/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 from .models import News
 
 
 def index(request):
-    news = News.objects.order_by("-created_at")
+    news = News.objects.all()
     context = {'news': news, 'title': 'Список новостей'}
-    return render(request, template_name='news/index.html', context=context) 
+    return render(request, template_name='news/index.html', context=context)
