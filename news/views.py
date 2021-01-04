f0010e05715db956a6abf35e7cea41f5184f878b
mnosinov <mnosinov@gmail.com>
2021-01-04 15:13:51 +0600

    view has been enhanced

--- news/views.py
+++ news/views.py
@@ -6,5 +6,5 @@ from .models import News
 
 def index(request):
     news = News.objects.order_by("-created_at")
-    return render(request, 'news/index.html', {'news': news, 
-                                               'title': 'Список новостей'})
+    context = {'news': news, 'title': 'Список новостей'}
+    return render(request, template_name='news/index.html', context=context) 
