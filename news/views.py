bab859e6ac243d63103bce73e9371a71d8b25d57
mnosinov <mnosinov@gmail.com>
2021-01-08 15:33:01 +0600

    categories list added to index.html

--- news/views.py
+++ news/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 
-from .models import News
+from .models import News, Category
 
 
 def index(request):
     news = News.objects.all()
-    context = {'news': news, 'title': 'Список новостей'}
+    categories = Category.objects.all()
+    context = {
+        'news': news,
+        'title': 'Список новостей',
+        'categories': categories,
+    }
     return render(request, template_name='news/index.html', context=context)
