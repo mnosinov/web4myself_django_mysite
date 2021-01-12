0ddcd7240376895deca3fa9ea229d5a64639b261
mnosinov <mnosinov@gmail.com>
2021-01-12 23:12:47 +0600

    NewsForm is changed from Form to ModelForm (now almost eveything is done by django itself - it is great!). In News model category field is changed - now it is required.

--- news/models.py
+++ news/models.py
@@ -13,7 +13,7 @@ class News(models.Model):
     is_published = models.BooleanField(default=True,
                                        verbose_name='Опубликовано')
     category = models.ForeignKey('Category', on_delete=models.PROTECT,
-                                 null=True, verbose_name='Категория')
+                                 verbose_name='Категория')
 
     def get_absolute_url(self):
         return reverse('view_news', kwargs={"news_id": self.pk})
