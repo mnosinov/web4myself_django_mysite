ec61dca2be17550b909847a9f38f4bceb2cd7689
mnosinov <mnosinov@gmail.com>
2021-01-12 14:30:44 +0600

    add news form added - form is unbounded to model. form elements in html are generated with form.as_p tag

--- news/urls.py
+++ news/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import index, get_category, view_news
+from .views import index, get_category, view_news, add_news
 
 
 urlpatterns = [
     path('', index, name='home'),
     path('category/<int:category_id>/', get_category, name='category'),
     path('news/<int:news_id>/', view_news, name='view_news'),
+    path('news/add_news/', add_news, name='add_news'),
 ]
