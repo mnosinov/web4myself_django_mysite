d72d72d73696d6108ee2ca7b8d9003ee4eef0c46
mnosinov <mnosinov@gmail.com>
2021-01-13 13:24:45 +0600

    allow_empty set to False - and now if we don't have news to show in news by category 404 page is shown

--- news/views.py
+++ news/views.py
@@ -25,6 +25,7 @@ class NewsByCategory(ListView):
     model = News
     template_name = 'news/home_news_list.html'
     context_object_name = 'news'
+    allow_empty = False
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
