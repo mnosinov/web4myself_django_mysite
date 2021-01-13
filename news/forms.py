32f203e3de3b7236aa8e6e7c1267508f0b4bf01e
mnosinov <mnosinov@gmail.com>
2021-01-13 11:55:56 +0600

    custom validation on field title has been added. Some postprocessing applied to the title - converting to upper case

--- news/forms.py
+++ news/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from .models import News
+import re
+from django.core.exceptions import ValidationError
 
 
 class NewsForm(forms.ModelForm):
@@ -14,3 +16,17 @@ class NewsForm(forms.ModelForm):
             'category': forms.Select(attrs={"class": "form-select"}),
 
         }
+
+    def clean_title(self):
+        """
+        this method is call after django validation process, during call of
+        form.is_valid().
+        After django
+        validation process dictionary cleaned_data is prepared and we can
+        do our ownd custom validation. Name of the method is conventional:
+            clean_<field name>
+        """
+        title = self.cleaned_data['title']
+        if re.match(r'\d', title):     # check if first letter in title is digit
+            raise ValidationError('Название не должно начинаться с цифры')
+        return title.upper()    # just for fun - we convert title to upper case
