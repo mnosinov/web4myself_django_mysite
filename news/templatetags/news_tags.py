45022dcd34d41d42ad820b223b1a562594d590b8
mnosinov <mnosinov@gmail.com>
2021-01-21 23:14:45 +0600

    visual badges with news quantity for each category have been added to html

--- news/templatetags/news_tags.py
+++ news/templatetags/news_tags.py
@@ -14,6 +14,6 @@ def get_categories():
 
 @register.inclusion_tag('news/list_categories.html')
 def show_categories(arg1='Hello', arg2='world'):
-    categories = Category.objects. \
-        annotate(Count('news')).filter(news__count__gt=0)
+    categories = Category.objects.\
+        annotate(cnt=Count('news')).filter(cnt__gt=0)
     return {"categories": categories, "arg1": arg1, "arg2": arg2}
