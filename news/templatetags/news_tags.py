ec7a81de47b862ede3de026e12584374627d5812
mnosinov <mnosinov@gmail.com>
2021-01-21 22:49:37 +0600

    annotate added to show_categories inclusion tag. Now only categories with existing news are shown in categories list on sidebar

--- news/templatetags/news_tags.py
+++ news/templatetags/news_tags.py
@@ -1,4 +1,5 @@
 from django import template
+from django.db.models import Count
 
 from news.models import Category
 
@@ -13,5 +14,6 @@ def get_categories():
 
 @register.inclusion_tag('news/list_categories.html')
 def show_categories(arg1='Hello', arg2='world'):
-    categories = Category.objects.all()
+    categories = Category.objects. \
+        annotate(Count('news')).filter(news__count__gt=0)
     return {"categories": categories, "arg1": arg1, "arg2": arg2}
