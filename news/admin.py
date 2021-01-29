487693667d4f93cf51af8836ec17797ce00209e0
werty <mnosinov@gmail.com>
2021-01-29 16:20:13 +0600

    photo is now displayed in admin panel in news list page

--- news/admin.py
+++ news/admin.py
@@ -1,15 +1,22 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
 from .models import News, Category
 
 
 class NewsAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'category', 'created_at', 'updated_at',
-                    'is_published', 'views')
+                    'is_published', 'views', 'get_photo')
     list_display_links = ('id', 'title')
     search_fields = ('title', 'content')
     list_editable = ('is_published', 'views')
     list_filter = ('is_published', 'category')
+
+    def get_photo(self, obj):
+        if obj.photo:
+            return mark_safe(f'<img src="{obj.photo.url}" width="75">')
+        else:
+            return 'Фото не установлено'
 
 
 class CategoryAdmin(admin.ModelAdmin):
