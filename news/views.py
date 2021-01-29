6a73147d84ae0d0e3a977fbfd9a38739253684cc
werty <mnosinov@gmail.com>
2021-01-29 18:53:51 +0600

    pagination enhanced in html template. Pagination also added to news by category list page

--- news/views.py
+++ news/views.py
@@ -42,6 +42,7 @@ class NewsByCategory(MyMixin, ListView):
     template_name = 'news/home_news_list.html'
     context_object_name = 'news'
     allow_empty = False
+    paginate_by = 2
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
