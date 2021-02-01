db1a4aeae320046ecdfa77d5d52b6c42cf91fdc0
werty <mnosinov@gmail.com>
2021-02-01 21:44:50 +0600

    low level caching applied for categories list on sidebar. One less SQL query now

--- news/templatetags/news_tags.py
+++ news/templatetags/news_tags.py
@@ -1,5 +1,6 @@
 from django import template
 from django.db.models import Count, F
+from django.core.cache import cache
 
 from news.models import Category
 
@@ -14,7 +15,16 @@ def get_categories():
 
 @register.inclusion_tag('news/list_categories.html')
 def show_categories(arg1='Hello', arg2='world'):
-    categories = Category.objects.\
-        annotate(cnt=Count('news', filter=F('news__is_published'))).\
+    # get_or_set - is alternative method, but is less readable
+    # categories = cache.get_or_set('categories', Category.objects.\
+    #     annotate(cnt=Count('news', filter=F('news__is_published'))).\
+    #     filter(cnt__gt=0), 30)
+    categories = cache.get('categories')
+
+    if not categories:
+        categories = Category.objects.\
+            annotate(cnt=Count('news', filter=F('news__is_published'))).\
             filter(cnt__gt=0)
+        cache.set('categories', categories, 30)
+
     return {"categories": categories, "arg1": arg1, "arg2": arg2}
