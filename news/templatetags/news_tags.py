e6a49284224095fdbd44ab15a3fadd6ad813b2ea
werty <mnosinov@gmail.com>
2021-02-01 21:45:51 +0600

    caching examples disabled - for development time we need actual data from database always

--- news/templatetags/news_tags.py
+++ news/templatetags/news_tags.py
@@ -15,16 +15,19 @@ def get_categories():
 
 @register.inclusion_tag('news/list_categories.html')
 def show_categories(arg1='Hello', arg2='world'):
-    # get_or_set - is alternative method, but is less readable
-    # categories = cache.get_or_set('categories', Category.objects.\
-    #     annotate(cnt=Count('news', filter=F('news__is_published'))).\
-    #     filter(cnt__gt=0), 30)
-    categories = cache.get('categories')
+    # # get_or_set - is alternative method, but is less readable
+    # # categories = cache.get_or_set('categories', Category.objects.\
+    # #     annotate(cnt=Count('news', filter=F('news__is_published'))).\
+    # #     filter(cnt__gt=0), 30)
+    # categories = cache.get('categories')
 
-    if not categories:
-        categories = Category.objects.\
-            annotate(cnt=Count('news', filter=F('news__is_published'))).\
-            filter(cnt__gt=0)
-        cache.set('categories', categories, 30)
+    # if not categories:
+    #     categories = Category.objects.\
+    #         annotate(cnt=Count('news', filter=F('news__is_published'))).\
+    #         filter(cnt__gt=0)
+    #    cache.set('categories', categories, 30)
+    categories = Category.objects.\
+        annotate(cnt=Count('news', filter=F('news__is_published'))).\
+        filter(cnt__gt=0)
 
     return {"categories": categories, "arg1": arg1, "arg2": arg2}
