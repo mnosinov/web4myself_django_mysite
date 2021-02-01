46b41b3d8568a7343b281402e1d3e2f10174bded
werty <mnosinov@gmail.com>
2021-02-01 15:36:43 +0600

    small beautification of code

--- news/forms.py
+++ news/forms.py
@@ -11,11 +11,11 @@ class UserRegisterForm(UserCreationForm):
                                help_text='Максимум 150 символов',
                                widget=forms.TextInput(attrs={"class": "form-control"}))
     password1 = forms.CharField(label='Пароль',
-                               widget=forms.PasswordInput(attrs={"class": "form-control"}))
+                                widget=forms.PasswordInput(attrs={"class": "form-control"}))
     password2 = forms.CharField(label='Подтверждение пароля',
-                               widget=forms.PasswordInput(attrs={"class": "form-control"}))
+                                widget=forms.PasswordInput(attrs={"class": "form-control"}))
     email = forms.EmailField(label='Email',
-                               widget=forms.EmailInput(attrs={"class": "form-control"}))
+                             widget=forms.EmailInput(attrs={"class": "form-control"}))
 
     class Meta:
         model = User
